c243898a7fcfbe9dfff206955f7b66f3e819cc92
Ivan Yang <ivan@ivans-mbp.lan>
2022-11-14 21:50:09 -0500

    Change to URI

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.get('/app/roll/', (req, res, next) => {
     res.send(roll(-1,-1,-1))
 })
 
+app.get('/app/roll/?sides=:sides&dice=:dice&rolls=:rolls/', (req, res, next) => {
+    res.send(roll(parseInt(req.params.sides),parseInt(req.params.dice),parseInt(req.params.rolls)))
+})
+
 app.get('/app/roll/:sides/', (req, res, next) => {
     res.send(roll(parseInt(req.params.sides),-1,-1))
 })
