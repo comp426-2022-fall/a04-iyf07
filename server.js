8a2d3a57e1609d12987ec854ec707a1b21dafc2b
Ivan Yang <ivan@ivans-mbp.lan>
2022-11-14 22:10:27 -0500

    encoded

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('*', (req, res) => {
 });
 
 app.post('/app/roll/', (req, res) => {
-    res.send(parseInt(req.body.sides),parseInt(req.body.dice),parseInt(req.body.rolls));
+    res.send(roll(parseInt(req.body.sides),parseInt(req.body.dice),parseInt(req.body.rolls)));
 });
 
 app.listen(port)
