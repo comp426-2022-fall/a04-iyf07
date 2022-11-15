ad4f4226e2b7ce3ef9a261d72bd9ceffe827c902
Ivan Yang <ivan@ivans-mbp.lan>
2022-11-14 21:39:02 -0500

    Change to int

--- server.js
+++ server.js
@@ -17,15 +17,15 @@ app.get('/app/roll/', (req, res, next) => {
 })
 
 app.get('/app/roll/:sides/', (req, res, next) => {
-    res.send(roll(req.params.sides,-1,-1))
+    res.send(roll(parseInt(req.params.sides),-1,-1))
 })
 
 app.get('/app/roll/:sides/:dice/', (req, res, next) => {
-    res.send(roll(req.params.sides,req.params.dice,-1))
+    res.send(roll(parseInt(req.params.sides),parseInt(req.params.dice),-1))
 })
 
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res, next) => {
-    res.send(roll(req.params.sides,req.params.dice,req.params.rolls))
+    res.send(roll(parseInt(req.params.sides),parseInt(req.params.dice),parseInt(req.params.rolls)))
 })
 
 app.get('*', (req, res) => {
