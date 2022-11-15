978cf2650a63b9045f319ba7c4161ec68fedebdd
Ivan Yang <ivan@ivans-mbp.lan>
2022-11-14 20:56:09 -0500

    a04

---
+++ server.js
@@ -0,0 +1,41 @@
+import roll from './lib/roll.js'
+import express from 'express'
+const app = express()
+const port = 5555
+// import minimist from 'minimist';
+
+//
+// const args = minimist(process.argv.slice(2))
+// let s=args.sides;
+// let d=args.dice;
+// let r=args.rolls;
+// const res = {sides: s, dice:d, rolls:roll(s,d,r)}
+// console.log(JSON.stringify(res))
+
+app.get('/app/', (req, res, next) => {
+    res.send("200 OK")
+})
+
+app.get('/app/roll/', (req, res, next) => {
+    res.send(roll(-1,-1,-1))
+})
+
+app.get('/app/roll/:sides/', (req, res, next) => {
+    res.send(roll(req.params.sides,-1,-1))
+})
+
+app.get('/app/roll/:sides/:dice/', (req, res, next) => {
+    res.send(roll(req.params.sides,req.params.dice,-1))
+})
+
+app.get('/app/roll/:sides/:dice/:rolls/', (req, res, next) => {
+    res.send(roll(req.params.sides,req.params.dice,req.params.rolls))
+})
+
+app.get('*', (req, res) => {
+    res.send("404 NOT FOUND");
+});
+
+app.listen(port, () => {
+    console.log("Server listening on port" + port)
+})
